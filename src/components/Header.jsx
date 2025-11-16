0569f46cc009003278d5d0a019491702807c0ff9
LuisFalco <101217187+LukeFalcox@users.noreply.github.com>
2025-11-16 22:14:57 +0000

    Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,50 +15,67 @@ const Header = ({ darkMode, toggleDarkMode }) => {
       <div className="container mx-auto px-4 py-4">
         <div className="flex justify-between items-center">
           <div className="flex items-center space-x-4">
-            <img
-              src="/public/folha.png"
-              className="w-10 h-10 md:w-12 md:h-12"
-            />
+            <img src="/folha.png" className="w-10 h-10 md:w-12 md:h-12" />
             <h1 className="text-2xl font-bold">GreenOffice</h1>
           </div>
-          {mobileOpen && (
-            <div className="mobile-menu fixed inset-0 bg-white z-30 md:hidden">
-              <div className="flex justify-between items-center p-6 border-b">
-                <span className="text-xl font-bold text-indigo-600">Menu</span>
-                <button className="text-gray-600" onClick={toggleMobileMenu}>
-                  <i className="fas fa-times text-xl"></i>
-                </button>
-              </div>
-              <nav className="flex flex-col p-6 space-y-6">
-                <Link to="/" className="text-gray-600 hover:text-indigo-600">
-                  Sobre mim
-                </Link>
-                <Link
-                  to="/projeto"
-                  className="text-gray-600 hover:text-indigo-600"
-                >
-                  Projetos
-                </Link>
-                <Link
-                  to="/contato"
-                  className="text-gray-600 hover:text-indigo-600"
-                >
-                  Contato
-                </Link>
-              </nav>
-            </div>
-          )}
+
+          <nav className="hidden md:flex space-x-8">
+            <Link
+              to="/"
+              className="text-gray-600 hover:text-indigo-600"
+              onClick={toggleMobileMenu}
+            >
+              Home
+            </Link>
+            <Link
+              to="/contribuidores"
+              className="text-gray-600 hover:text-indigo-600"
+              onClick={toggleMobileMenu}
+            >
+              Contribuidores
+            </Link>
+          </nav>
+
           <div className="flex items-center space-x-4">
             <button className="md:hidden" onClick={toggleMobileMenu}>
               <i className="fas fa-bars text-2xl"></i>
             </button>
-            <button onClick={toggleDarkMode}> {darkMode ? "☀️" : "🌙"} </button>
+
+            <button onClick={toggleDarkMode}>{darkMode ? "☀️" : "🌙"}</button>
+
             <button className="px-4 py-2 rounded-lg font-semibold bg-green-600 text-white">
               Entrar
             </button>
           </div>
         </div>
       </div>
+
+      {mobileOpen && (
+        <div className="mobile-menu fixed inset-0 bg-white z-30 md:hidden">
+          <div className="flex justify-between items-center p-6 border-b">
+            <span className="text-xl font-bold text-indigo-600">Menu</span>
+            <button className="text-gray-600" onClick={toggleMobileMenu}>
+              <i className="fas fa-times text-xl"></i>
+            </button>
+          </div>
+          <nav className="flex flex-col p-6 space-y-6">
+            <Link
+              to="/"
+              className="text-gray-600 hover:text-indigo-600"
+              onClick={toggleMobileMenu}
+            >
+              Home
+            </Link>
+            <Link
+              to="/contribuidores"
+              className="text-gray-600 hover:text-indigo-600"
+              onClick={toggleMobileMenu}
+            >
+              Contribuidores
+            </Link>
+          </nav>
+        </div>
+      )}
     </header>
   );
 };
